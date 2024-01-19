3d75e7995b70e2283c93ed0a456e04e5f96516e4
AbuDzarAlGhifari <rasduba@gmail.com>
2024-01-19 08:10:45 +0700

    perbaiki mysql2

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,20 +1,28 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import mysql2 from 'mysql2'
 
 dotenv.config();
 
 const db = new Sequelize({
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
   dialect: process.env.DB_DIALECT,
+  dialectModule: mysql2,
+  benchmark: true
 });
 
 (async () => {
   try {
     await db.authenticate();
     console.log('Connected to the database');
+
+    // Use sync() or sync({ alter: true }) to synchronize the database
+    await db.sync({ alter: true });
+
+    console.log('Database synchronized successfully');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
